b3a955803ec1b84d03c1d96bace63b77df137b2f
jaynarw <jnarwaria@gmail.com>
2020-05-16 19:03:28 +0530

    Initial commit

---
+++ Extension/src/index.js
@@ -0,0 +1,8 @@
+let x = require('readability-node');
+console.log(x);
+import React from 'react';
+const name = "Jaus";
+import ReactDOM from 'react-dom';
+ReactDOM.render(
+    <h1>{name}</h1>, document
+)
