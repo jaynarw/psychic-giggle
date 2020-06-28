60bd3e41dbac443ab9a825f998c5a3447a570654
jaynarw <jnarwaria@gmail.com>
2020-06-28 16:15:30 +0530

    Fix onClick on enable voice

--- Extension/src/js/components/VoiceChatter.js
+++ Extension/src/js/components/VoiceChatter.js
@@ -22,6 +22,7 @@ class VoiceChatter extends React.Component {
     };
     const { liveCalls } = this.props;
     const clickedUsername = evt.target.getAttribute('name');
+    console.log(evt.target);
     if (liveCalls[clickedUsername]) {
       console.log("You can't start a call because you already have one open!");
       // return;
@@ -30,7 +31,7 @@ class VoiceChatter extends React.Component {
         console.log("I'm afraid I can't let you talk to yourself. That would be weird.");
         // return;
       }
-
+      console.log(`clicked ${clickedUsername}`);
       const newPeer = new RTCPeer(clickedUsername, this.audioSocket, document.getElementById(`audio-${clickedUsername}`));
       navigator.mediaDevices.getUserMedia(mediaConstraints)
         .then((localStream) => {
@@ -114,9 +115,9 @@ class VoiceChatter extends React.Component {
         {onlineUsers.flatMap((user) => (
           (user.id === socket.id) ? []
             : [
-              <li className="active-user" key={user.id} name={user.id} onClick={(event) => this.invite(event)}>
+              <li className="active-user" key={user.id}>
                 {user.nickname}
-                <span className="enable-voice">Enable voice</span>
+                <span className="enable-voice" name={user.id} onClick={(event) => this.invite(event)}>Enable voice</span>
               </li>]
         ))}
         {onlineUsers.flatMap((user) => (
