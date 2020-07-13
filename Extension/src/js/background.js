0f8104f06d6d1edb2c79d1ac8b92a6391f7d7ff1
jaynarw <jnarwaria@gmail.com>
2020-07-13 20:32:03 +0530

    Fix bug in content script Netflix

--- Extension/src/js/background.js
+++ Extension/src/js/background.js
@@ -26,25 +26,34 @@ chrome.runtime.onInstalled.addListener((details) => {
     }]);
   });
   chrome.pageAction.onClicked.addListener(() => {
+    console.log('Clicked');
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       const currentTabURL = new URL(tabs[0].url);
       chrome.tabs.sendMessage(tabs[0].id, { type: 'existenceIs' }, (response) => {
+        console.log('Sending message');
         if (typeof response === 'undefined') {
+          console.log('No script');
           if (chrome.runtime.lastError) {
+            console.log('Injecting script 1');
             chrome.tabs.executeScript({ file: 'js/scriptInjectedChecker.js' });
             if (currentTabURL.hostname.indexOf('.primevideo.') !== -1) {
+              console.log('Injecting prime');
               chrome.tabs.executeScript({ file: 'js/content.js' });
             } else if (currentTabURL.hostname.indexOf('.netflix.') !== -1) {
+              console.log('Injecting netflix');
               chrome.tabs.executeScript({ file: 'js/contentNetflix.js' });
             }
           }
         } else if (response.type === 'pain') { /* Checks if content script is already injected */
           console.log('Already injected');
         } else {
+          console.log('Injecting script 2');
           chrome.tabs.executeScript({ file: 'js/scriptInjectedChecker.js' });
           if (currentTabURL.hostname.indexOf('.primevideo.') !== -1) {
+            console.log('Injecting prime');
             chrome.tabs.executeScript({ file: 'js/content.js' });
           } else if (currentTabURL.hostname.indexOf('.netflix.') !== -1) {
+            console.log('Injecting netflix');
             chrome.tabs.executeScript({ file: 'js/contentNetflix.js' });
           }
         }
