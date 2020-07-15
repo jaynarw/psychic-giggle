a6f54aef5d91d224113f0d065b34d60fb133a241
jaynarw <jnarwaria@gmail.com>
2020-07-16 00:46:35 +0530

    Fix chatbox insertion on click

--- Extension/src/js/contentScriptNetflix.js
+++ Extension/src/js/contentScriptNetflix.js
@@ -7,19 +7,20 @@ chatBoxContainer.id = 'psychic-giggler';
 chatBoxContainer.style.zIndex = '9';
 chatBoxContainer.style.top = '0';
 
-const url = new URL(window.location.href);
-const prefix = url.pathname.split('/')[1];
-const videoPlayerContainer = document.querySelector('.NFPlayer.nf-player-container');
-if (prefix === 'watch' && videoPlayerContainer) {
-  videoPlayerContainer.style.setProperty('width', '80%', 'important');
-}
+// const url = new URL(window.location.href);
+// const prefix = url.pathname.split('/')[1];
+// const videoPlayerContainer = document.querySelector('.NFPlayer.nf-player-container');
+// if (prefix === 'watch' && videoPlayerContainer) {
+//   videoPlayerContainer.style.setProperty('width', '80%', 'important');
+// }
 
 function check() {
   const url = new URL(window.location.href);
   const prefix = url.pathname.split('/')[1];
   if (prefix === 'watch') {
     const videoPlayerContainer = document.querySelector('.NFPlayer.nf-player-container');
-    const nowPlayingElt = videoPlayerContainer.querySelector('.video-title');
+    let nowPlayingElt;
+    if (videoPlayerContainer) nowPlayingElt = videoPlayerContainer.querySelector('.video-title');
     const sizingWrapper = document.querySelector('.sizing-wrapper');
     if (videoPlayerContainer
       && nowPlayingElt
@@ -35,7 +36,7 @@ function check() {
   }
 }
 
-
+check();
 const config = { attribute: false, childList: true, subtree: true };
 const obs = new MutationObserver(check);
 obs.observe(document.body, config);
