255fdb696b838150b346e42c8f324a229ae04af3
jaynarw <jnarwaria@gmail.com>
2020-07-18 18:03:30 +0530

    Update colors & styling of button

--- Extension/src/js/components/Header.js
+++ Extension/src/js/components/Header.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import Logo from './Logo';
 
 function Header(props) {
-  const imageSrc = chrome.runtime.getURL('img/minimal.png');
+  const imageSrc = chrome.runtime.getURL('img/minimal-desaturated.svg');
   function hoverLogo(e) {
     const ref = document.querySelector('.logo-bingebox');
     ref.classList.remove('animated');
@@ -15,11 +16,12 @@ function Header(props) {
         rel="stylesheet"
       />
       <div className="logo-bingebox" onMouseEnter={hoverLogo}>
-        <img
+        {/* <svg
           className="popcorn-logo"
           src={imageSrc}
           alt="Bingebox Logo"
-        />
+        /> */}
+        <Logo />
         <span className="binge">binge</span>
         <span className="box">box</span>
       </div>
